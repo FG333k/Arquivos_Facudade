d12cc39c3bb4561e85e5eb95f7a756904428cf47
Felipe Almeida <felipefeturbo@gmail.com>
2025-05-14 16:30:57 -0300

    Melhorias código aplicação

--- Tecnologias/Linguagem_python/paaplicação/index.py
+++ Tecnologias/Linguagem_python/paaplicação/index.py
@@ -27,19 +27,15 @@ rframe.pack(side="right") # Indica a posição na janela
 
 #Input login
 loginl = Label(rframe, text="login", fg="white", bg="black") # insere uma label 
-loginl.place(x=10, y=140) # Exibe a label na posição determinada 
+loginl.place(x=10, y=100) # Exibe a label na posição determinada 
 logini = Entry(rframe, width=35) # Insere uma caixa de texto 
-logini.place(x=50, y=140) # Exibe a caixa de texto na posição determinada
+logini.place(x=50, y=100) # Exibe a caixa de texto na posição determinada
 
 #Input senha
 senhal = Label(rframe, text="Sanha", fg="white", bg="black") # insere uma label
-senhal.place(x=7, y=170) # Exibe a label na posição determinada
+senhal.place(x=7, y=140) # Exibe a label na posição determinada
 senhai = Entry(rframe, width=35) # Insere uma caixa de texto
-senhai.place(x=50, y=170) # Exibe a caixa de texto na posição determinada
-
-#Caixa com as listas de cadastros
-listbox = Listbox(rframe, width=45, height=7) # Insere uma caixa de listas
-listbox.place(x=5, y=6) # Exibe na posição determinada 
+senhai.place(x=50, y=140) # Exibe a caixa de texto na posição determinada
 
 # Função login
 def Login():
@@ -54,54 +50,91 @@ def Login():
     dados = banco.sql.fetchone() # Seeciona apenas uma linha no banco de dados
 
     # Condições para login
-    try:
-        if(login in dados and senha in dados):
-            messagebox.showinfo(title='Login', message='Seja bem vindo: '+login) # Exibe uma mensagem de boas vindas com o nome do login
+    if (login == '' or senha == ''):
+        messagebox.showwarning(title="Atenção", message='Preencha todos os capos')
+    else:
+        try:
+
+            if(login in dados and senha in dados):
+                messagebox.showinfo(title='Login', message='Seja bem vindo: '+login) # Exibe uma mensagem de boas vindas com o nome do login
+
+                # Limpa os campos antes preenchidos
+                logini.delete(0,END)
+                senhai.delete(0,END)
+        except:
+            messagebox.showinfo(title='Login', message='Usuário inexistente! ') # Exibe uma mesagem de erro
 
             # Limpa os campos antes preenchidos
             logini.delete(0,END)
             senhai.delete(0,END)
-    except:
-        messagebox.showinfo(title='Login', message='Usuário inexistente! ') # Exibe uma mesagem de erro
-
-        # Limpa os campos antes preenchidos
-        logini.delete(0,END)
-        senhai.delete(0,END)
 
 #Botão de login
-loginbt = Button(rframe, text="Logar", width=10, command=Login) # Cria o botão para logar no sistema
-loginbt.place(x=40, y=220) # Exibe na posição determinada
+loginbt = Button(rframe, text="Logar", width=8, command=Login) # Cria o botão para logar no sistema
+loginbt.place(x=60, y=220) # Exibe na posição determinada
 
 #Função de cadastro
 def cadastro():
 
     # Reposicionando elementos na tela 
-    #Campo login
-    logini.place(x=50, y=100)
-    loginl.place(x=10, y=100) 
-    #Campo list box
-    listbox.place(x=3000)
+    #Lado esquerdo
+    #Titulo
+    titulo.configure(text="CADASTROS")
+    titulo.place(x=90)
+    #Logo
+    logol.place(x=-3000)
+
+    #Lado direito
+    loginl.place(x=10, y=130)
+    logini.place(x=50, y=130)
     #Campo senha
-    senhai.place(x=50, y=140)
-    senhal.place(x=10, y=140)   
+    senhal.place(x=7, y=160)
+    senhai.place(x=50, y=160)   
     #Botão login
     loginbt.place(x=3000)
     #Botão cadastro
     cadbt.place(x=3000)
-    #Botão listar
-    listbt.place(x=3000)
     
     # Imput nome 
     nomel = Label(rframe, text="Nome", bg="black", fg="white") # insere uma labe
-    nomel.place(x=5, y=20) # Exibe a label na posição determinada
+    nomel.place(x=5, y=70) # Exibe a label na posição determinada
     nomei = Entry(rframe, width=35) # Insere uma caixa de texto
-    nomei.place(x=50, y=20) # Exibe a caixa de texto na posição determinada
+    nomei.place(x=50, y=70) # Exibe a caixa de texto na posição determinada
 
     # Imput email
     emaill = Label(rframe, text="Email", bg="black", fg="white") # insere uma labe
-    emaill.place(x=5, y=60) # Exibe a label na posição determinada
+    emaill.place(x=5, y=100) # Exibe a label na posição determinada
     emaili = Entry(rframe, width=35) # Insere uma caixa de texto
-    emaili.place(x=50, y=60) # Exibe a caixa de texto na posição determinada
+    emaili.place(x=50, y=100) # Exibe a caixa de texto na posição determinada
+
+    
+    #Caixa com as listas de cadastros
+    listbox = Listbox(lframe, width=45, height=11) # Insere uma caixa de listas
+    listbox.insert(END, "LISTA DE CADASTROS:")
+    listbox.place(x=20, y=70) # Exibe na posição determinada 
+
+    # Função para listar os usuários/cadastros na box list
+    def listar():
+        # Limpa o campo da list box
+        listbox.delete(0, END)
+
+        # Selecionando as variaveis na lista do banco de dados 
+        banco.sql.execute("Select nome,email,login from login")
+        usuarios = banco.sql.fetchall() # Seleciona cada linha do bamco de dados
+
+        # Verifica se há usuários cadastrados 
+        if usuarios:
+            # Título da list box
+            listbox.insert(END, "LISTA DE CADASTROS:")
+            # Printa ususário por usuário na list box
+            for usuario in usuarios:
+                nome,email,login = usuario
+                listbox.insert(END, f"Nome: {nome} | Email: {email} | Login: {login}") # Exíbe a mensagem na list box formatado
+        else:
+            listbox.insert(END, "Nenhum usuário cadastrado!") # Exibe uma mensagem de aviso na list box
+
+    # Botão para listar cadstros/usuários
+    listbt = Button(rframe, text="Listar", width=8, command=listar)# Cria o botão para listar usuários/cadastros na list box
+    listbt.place(x=40, y=220)# Exibe na posição determinada
 
     # Função para inserir um cadastro no banco de dados
     def inserir():
@@ -133,12 +166,20 @@ def cadastro():
             messagebox.showinfo(title='Cadasro', message='Cadastro realizado com sucesso') # Exibição da mensagem de confirmação
 
     # Botão de inserir
-    inserirbt = Button(rframe, text="Adicionar", width=10, command=inserir) # Cria o botão para inserir cadastros 
-    inserirbt.place(x=40, y=220) # Exibe na posição determinada
+    inserirbt = Button(rframe, text="Adicionar", width=8, command=inserir) # Cria o botão para inserir cadastros 
+    inserirbt.place(x=112, y=220) # Exibe na posição determinada
 
     # Função para voltar
     def voltar():
         # Reposicionando elementos na tela
+        #Lado esquerdo
+        #Título
+        titulo.configure(text="HELO-WORD")
+        titulo.place(x=100)
+        #Logo
+        logol.place(x=40, y=50)
+
+        #Lado direirto
         #Campo nome
         nomei.place(x=3000)
         nomel.place(x=3000)
@@ -148,50 +189,30 @@ def cadastro():
         #Campo list box
         listbox.place(x=5)
         #Campo login
-        loginl.place(x=10, y=130)
-        logini.place(x=50, y=130)
+        loginl.place(x=10, y=100)
+        logini.place(x=50, y=100)
         #Campo senha
-        senhal.place(x=7, y=160)
-        senhai.place(x=50, y=160)
+        senhal.place(x=7, y=140)
+        senhai.place(x=50, y=140)
+        #Caixa com as listas de cadastros
+        listbox.place(x=-3000)
+        #Botão listar
+        listbt.place(x=3000)
         #Botão inserir
         inserirbt.place(x=3000)
         #Botão voltar
         voltarbt.place(x=3000)
         #Botão login
-        loginbt.place(x=40)
+        loginbt.place(x=60)
         #Botão cadartro
-        cadbt.place(x=110)
-        #Botão listar
-        listbt.place(x=180)
+        cadbt.place(x=180)
 
     #Botão para voltar
-    voltarbt = Button(rframe, text="Voltar", width=10, command=voltar) # Cria o botão para retornar a pagina tela inicial
-    voltarbt.place(x=180, y=220) # Exibe na posição determinada
+    voltarbt = Button(rframe, text="Voltar", width=8, command=voltar) # Cria o botão para retornar a pagina tela inicial
+    voltarbt.place(x=182, y=220) # Exibe na posição determinada
 
 # Botaõ de cadastro
-cadbt = Button(rframe, text="Cadastrar", width=10, command=cadastro) # Cria o botão para acessar o formulário de cadarstros
-cadbt.place(x=110, y=220) # Exibe na posição determinada
-
-# Função para listar os usuários/cadastros na box list
-def listar():
-    # Limpa o campo da list box
-    listbox.delete(0, END)
-
-    # Selecionando as variaveis na lista do banco de dados 
-    banco.sql.execute("Select nome,email,login from login")
-    usuarios = banco.sql.fetchall() # Seleciona cada linha do bamco de dados
-
-    # Verifica se há usuários cadastrados 
-    if usuarios:
-        # Printa ususário por usuário na list box
-        for usuario in usuarios:
-            nome,email,login = usuario
-            listbox.insert(END, f"Nome: {nome} | Email: {email} | Login: {login}") # Exíbe a mensagem na list box formatado
-    else:
-        listbox.insert(END, "Nenhum usuário cadastrado!") # Exibe uma mensagem de aviso na list box
-
-# Botão para listar cadstros/usuários
-listbt = Button(rframe, text="Listar", width=10, command=listar)# Cria o botão para listar usuários/cadastros na list box
-listbt.place(x=180, y=220)# Exibe na posição determinada
+cadbt = Button(rframe, text="Cadastrar", width=8, command=cadastro) # Cria o botão para acessar o formulário de cadarstros
+cadbt.place(x=180, y=220) # Exibe na posição determinada
 
 jan.mainloop() # Mantem a janela aberta
