a68cd7405a4873b85b37b02693db05078a7de721
Felipe Almeida <felipefeturbo@gmail.com>
2025-05-25 19:38:34 -0300

    Estudos em Java

---
+++ Tecnologias/JAVA/Main.java
@@ -0,0 +1,20 @@
+public class Main {   // O código sempre deve começar com uma classe e o nome dela deve ser o nome do arquivo
+  public static void main(String[] args) {    // Também deve haver sempre um main
+
+    // Exiibindo palavras no console
+    System.out.println("Olá mundo");
+    System.out.println("Estou aprendendo Java!");
+    System.out.println("System.out.println(Olá mundo!);");
+
+    System.out.println(".");
+
+    // Exibindo números no console
+    System.out.println(3);
+    System.out.println(358);
+    System.out.println(50000);
+
+    // Calculos matemáticos
+    System.out.println(2 + 3);
+    System.out.println(2 * 5);
+  }
+}
---
+++ Tecnologias/JAVA/Variaveis.java
@@ -0,0 +1,35 @@
+public class Variaveis {
+    public static void main(String[] args){
+
+        // Atribuição de variaveis      sintaxe:  tipo + espaço + nome + = + valor
+        // Variavel do tipo string
+        String nome = "Felipe ";     
+        System.out.println(nome);
+
+        // Também é possível mesclar variaveis string
+        String sobrenome = "Almeida";
+        System.out.println(nome + sobrenome);
+
+        // Variavel do tipo numero inteiro
+        int numero = 15;
+        System.out.println(numero);
+
+        // Atribuição de valor depois da declaração
+        int outroNumero;
+        outroNumero = 20;
+        System.out.println(outroNumero);
+
+        // Constantes
+        //final int const = 100;
+        //System.out.println(const);
+
+        // Outros tipos:
+        float numFloat = 5.99f;
+        char letra = 'A';
+        boolean boleano = true;
+        System.out.println(numFloat);
+        System.out.println(letra);
+        System.out.println(boleano);
+
+    }
+}
